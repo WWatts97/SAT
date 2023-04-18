bf68a7b20dc636b1114514a2a7d451b984ac6a62
WWatts97 <awnewatts97@outlook.com>
2023-04-17 20:41:33 -0500

    WELP

--- SAT.DATA.EF/Metadata/Metadata.cs
+++ SAT.DATA.EF/Metadata/Metadata.cs
@@ -5,49 +5,69 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SAT.DATA.EF
+namespace SAT.DATA.EF.Models
 {
     public class StudentsMetadata
     {
         public int StudentId { get; set; }
 
         [Required]
+        [StringLength(20)]
+        [Display(Name = "First Name")]
         public string? FirstName { get; set; }
 
         [Required]
+        [StringLength(20)]
+        [Display(Name = "Last Name")]
         public string? LastName { get; set; }
 
         [Required]
+        [StringLength(15)]
         public string Major {  get; set; }
 
         [Required]
+        [StringLength(50)]
         public string Address { get; set; }
 
         [Required]
+        [StringLength(25)]
         public string City { get; set; }
 
         [Required]
+        [StringLength(2)]
         public string State { get; set; }
 
         [Required]
+        [DataType(DataType.PostalCode)]
+        [StringLength(10)]
+        [Display(Name = "Zip")]
         public string ZipCode { get; set; }
 
+        [DataType(DataType.PhoneNumber)]
+        [StringLength(13)]
+        [Display(Name = "Phone Number")]
         public string Phone { get; set; }
 
+        [DataType(DataType.EmailAddress)]
+        [StringLength(60)]
         public string? Email { get; set; }
 
         [Required]
+        [StringLength(100)]
+        [Display(Name = "Image")]
         public string PhotoUrl { get; set; }
-
     }
 
     public class StudentStatusesMetadata
     {
         public int SSID { get; set; }
 
+        [Required]
+        [StringLength(30)]
         public string SSName { get; set; }
 
         [Required]
+        [StringLength(250)]
         public string SSDecription { get; set; }
     }
 
@@ -61,8 +81,10 @@ namespace SAT.DATA.EF
 
         public DateTime EndDate { get; set; }
 
+        [StringLength(40)]
         public string InstructorName { get; set; }
 
+        [StringLength(20)]
         public string Location { get; set; }
 
         public int SCSID { get; set; }
@@ -83,6 +105,7 @@ namespace SAT.DATA.EF
     {
         public int SCSID { get; set; }
 
+        [StringLength(50)]
         public string SCSName { get; set; }
 
     }
@@ -93,16 +116,20 @@ namespace SAT.DATA.EF
         public int CourseId { get; set; }
 
         [Required]
+        [StringLength(50)]
         public string CourseName { get; set; }
 
         [Required]
+        [StringLength(int.MaxValue)]
         public string CourseDescription { get; set; }
 
         [Required]
         public int CreditHours { get; set; }
 
+        [StringLength(250)]
         public string Curriculum { get; set; }
 
+        [StringLength(500)]
         public string Notes { get; set; }
 
         [Required]
--- SAT.DATA.EF/Metadata/Partials.cs
+++ SAT.DATA.EF/Metadata/Partials.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Update;
+using SAT.DATA.EF.Models;
+using Microsoft.AspNetCore.Mvc;
 
-namespace SAT.DATA.EF
+namespace SAT.DATA.EF.Models
 {
-    internal class Partials
-    {
-    }
+    [ModelMetadataType(typeof(CoursesMetadata))]
+    public partial class Course { }
+
+    [ModelMetadataType(typeof(EnrollmentsMetadata))]
+    public partial class Enrollment { }
+
+    [ModelMetadataType(typeof(ScheduledClassesMetadata))]
+    public partial class ScheduledClass { }
+
+    [ModelMetadataType(typeof(ScheduledClassStatusesMetadata))]
+    public partial class ScheduledClassStatus { }
+
+    [ModelMetadataType(typeof(StudentsMetadata))]
+    public partial class Student { }
+
+    [ModelMetadataType(typeof(StudentStatusesMetadata))]
+    public partial class StudentStatus { }
 }
