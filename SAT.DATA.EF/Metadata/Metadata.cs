1f763d2c56a04f9b567f5577d5802fbf4f4c1f97
WWatts97 <awnewatts97@outlook.com>
2023-04-18 19:30:11 -0500

    GANG

--- SAT.DATA.EF/Metadata/Metadata.cs
+++ SAT.DATA.EF/Metadata/Metadata.cs
@@ -56,6 +56,12 @@ namespace SAT.DATA.EF.Models
         [StringLength(100)]
         [Display(Name = "Image")]
         public string PhotoUrl { get; set; }
+
+        [Display(Name = "Status")]
+        public int Ssid { get; set; }
+
+        [Display(Name = "Status")]
+        public virtual StudentStatus Ss { get; set; }
     }
 
     public class StudentStatusesMetadata
@@ -64,41 +70,57 @@ namespace SAT.DATA.EF.Models
 
         [Required]
         [StringLength(30)]
-        public string SSName { get; set; }
+        [Display(Name ="Status")]
+        public string Ssname { get; set; }
 
         [Required]
         [StringLength(250)]
-        public string SSDecription { get; set; }
+        [Display(Name = "Status Description")]
+        public string Ssdescription { get; set; }
     }
 
     public class ScheduledClassesMetadata
     {
         public int ScheduledClassId { get; set; }
 
+        [Display(Name ="Course")]
         public int CourseId { get; set; }
 
+        [Display(Name = "Start Date")]
         public DateTime StartDate { get; set; }
 
+        [Display(Name = "End Date")]
         public DateTime EndDate { get; set; }
 
         [StringLength(40)]
+        [Display(Name = "Instructor Name")]
         public string InstructorName { get; set; }
 
         [StringLength(20)]
         public string Location { get; set; }
 
-        public int SCSID { get; set; }
+        [Display(Name = "Class Status")]
+        public int Scsid { get; set; }
+
+        [Display(Name = "Class Status")]
+        public virtual ScheduledClassStatus? Scs { get; set; }
     }
 
     public class EnrollmentsMetadata
     {
         public int EnrollmentId { get; set; }
 
+        [Display(Name = "Student ID")]
         public int StudentId { get; set; }
 
+        [Display(Name = "Scheduled Class")]
         public int ScheduledClassId { get; set; }
 
+        [Display(Name ="Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name = "Scheduled Class")]
+        public virtual ScheduledClass ScheduledClass { get; set; }
     }
 
     public class ScheduledClassStatusesMetadata
@@ -106,7 +128,8 @@ namespace SAT.DATA.EF.Models
         public int SCSID { get; set; }
 
         [StringLength(50)]
-        public string SCSName { get; set; }
+        [Display(Name = "Class Status")]
+        public string Scsname { get; set; }
 
     }
 
@@ -117,13 +140,16 @@ namespace SAT.DATA.EF.Models
 
         [Required]
         [StringLength(50)]
+        [Display(Name = "Course")]
         public string CourseName { get; set; }
 
         [Required]
         [StringLength(int.MaxValue)]
+        [Display(Name ="Course Description")]
         public string CourseDescription { get; set; }
 
         [Required]
+        [Display(Name = "Credit Hours")]
         public int CreditHours { get; set; }
 
         [StringLength(250)]
@@ -133,6 +159,7 @@ namespace SAT.DATA.EF.Models
         public string Notes { get; set; }
 
         [Required]
+        [Display(Name = "Active?")]
         public bool IsActive { get; set; }
     }
 
